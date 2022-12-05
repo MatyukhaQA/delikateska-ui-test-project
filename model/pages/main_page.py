86544a5b9ea906d5a77a74df0719d94a09d8ab66
matyukha <m@matyuha.ru>
2022-12-05 21:16:23 +0300

    fixed mistakes

--- model/pages/main_page.py
+++ model/pages/main_page.py
@@ -6,9 +6,9 @@ from selene.support.shared import browser
 
 load_dotenv()
 
-LOGIN = os.getenv('login')
-PASSWORD = os.getenv('password')
-URL = os.getenv('url')
+LOGIN = os.getenv('LOGIN')
+PASSWORD = os.getenv('PASSWORD')
+URL = os.getenv('URL')
 
 
 class MainPage:
